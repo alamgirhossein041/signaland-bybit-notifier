8e363d3d10a3218ceeaeb68d51fa2724f42bf762
Angelo Lippolis <info@angelolippolis.com>
2022-05-04 19:05:40 +0200

    Bybit harvester - init

---
+++ bybit-harvester/src/lib/user.rs
@@ -0,0 +1,144 @@
+pub mod user {
+
+    use chrono::{DateTime, Utc};
+    use serde::{Serialize, Deserialize};
+    use serde_json::Value;
+    use std::{collections::HashMap, hash::Hash};
+    use serde_dynamo::from_items;
+
+    use signaland_rust_lib::signaland::{services::aws::dynamo::{
+        DyVariables, DyNames, string_into_av, i64_into_av, query,
+        column_as_f64, column_as_string, generate_put_insert,
+        insert_rows, column_as_i64, TableCandles
+    }, strategies::Strategies};
+
+    const FUNCTION_NAME_PREFIX: &'static str = "signaland-user-executor-";
+    const USERS_TABLE_NAME: &'static str = "signaland_users";
+
+    #[derive(Serialize, Deserialize, Clone, Debug, Default)]
+    pub struct User {
+        pub id: String,
+        pub created_at: String,
+        pub data: Value,
+        pub email: String,
+        #[serde(default)]
+        pub free_subscription: bool,
+        pub payment_address: String,
+        pub settings: UserSettings,
+        pub subscriptions: Vec<String>,
+        pub subscription_expires_on: String,
+        #[serde(default)]
+        pub subscription_type: String,
+        #[serde(default)]
+        pub telegram_account: String,
+        pub updated_at: String,
+        pub verified: bool
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug, Default)]
+    pub struct UserSettings {
+        pub streams: Vec<UserStream>
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug, Default)]
+    pub struct UserStream {
+        pub id: f64,
+        pub created_at: String,
+        pub active: bool,
+        pub description: String,
+        pub exchange: String,
+        pub opt: OptValues,
+        #[serde(default)]
+        pub pairs: Value,
+        pub receivers: StreamReceivers,
+        pub strategy: String,
+        pub title: String
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug, Default)]
+    pub struct OptValues {
+        #[serde(default)]
+        pub children: bool,
+        #[serde(default)]
+        pub leverage: f32,
+        #[serde(default)]
+        pub order_size: f32,
+        #[serde(default)]
+        pub pyramiding: usize
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug, Default)]
+    pub struct StreamReceivers {
+        #[serde(rename(deserialize = "3commas"))]
+        #[serde(default)]
+        pub threecommas: ThreeCommasReceiver,
+        #[serde(default)]
+        pub telegram: String,
+        #[serde(default)]
+        pub bybit: ByBitReceiver 
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug, Default)]
+    pub struct ThreeCommasReceiver {
+        bot_id: i64,
+        email_token: String
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug, Default)]
+    pub struct ByBitReceiver {
+        pub api_key: String,
+        pub api_secret: String
+    }
+
+    impl User {
+        pub fn get_streams(&self, strategy: Strategies) -> Vec<UserStream> {
+            self.settings.streams
+                .clone()
+                .into_iter()
+                .filter(|x| x.strategy == strategy.to_str())
+                .collect()
+        }
+
+        pub fn has_subscription_expired(&self) -> bool {
+            self.subscription_expires_on.parse::<DateTime<Utc>>().unwrap() < Utc::now()
+        }
+    }
+
+    pub async fn get_all_pro_users() -> Result<Vec<User>, &'static str> {
+
+        let ask: &str = "subscription_type = :subtype";
+
+        let vars: DyVariables = HashMap::from([ (":subtype".to_string(), string_into_av("pro")) ]);
+
+        let query_output = query(
+            USERS_TABLE_NAME,
+            ask,
+            vars,
+            None,
+            None,
+            Some(String::from("subscription_type-created_at-index"))
+        ).await;
+
+        let res = query_output.items;
+        if res.is_none() { return Err("Error while querying for pro users ") }
+
+        let items: Vec<User> = from_items(res.unwrap()).unwrap();
+
+        Ok(items)
+    }
+
+    pub async fn get_users_bybit_streams() -> HashMap<(String, String), (String, String)> {
+        let users = get_all_pro_users().await.unwrap_or(vec![]);
+        let mut result: HashMap<(String, String), (String, String)> = HashMap::new();
+
+        for user in users {
+            for stream in user.settings.streams {
+                if stream.exchange != "BYBIT" { continue; }
+                result.insert((user.id.clone(), format!("{}", stream.id)), (stream.receivers.bybit.api_key.clone(), stream.receivers.bybit.api_secret.clone()));
+            }
+        }
+
+        result
+    }
+
+}
--- bybit-harvester/src/main.rs
+++ bybit-harvester/src/main.rs
@@ -1,3 +1,105 @@
-fn main() {
-    println!("Hello, world!");
+mod lib;
+
+use std::{
+    thread, time, collections::HashMap, 
+    sync::mpsc::{SyncSender, sync_channel},
+    sync::{Arc, Mutex}
+};
+
+use lib::user::user::get_users_bybit_streams;
+
+use signaland_rust_lib::signaland as signaland;
+use signaland_rust_lib::signaland::services::aws::kinesis::push as push_to_kinesis;
+use signaland::services::exchange::bybit::{
+    api::websocket::AuthenticatedDerivativesAccount
+};
+
+use serde_json::to_string;
+
+type Connections = Arc<Mutex<HashMap<(String, String), AuthenticatedDerivativesAccount>>>;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+
+    // User hashmap Schema => HASH<(UserID, StreamId), (ApiKey, ApiSecret)>
+    let connections: Connections = Arc::new(Mutex::new(HashMap::new()));
+
+    // Populate users
+    let users = Arc::clone(&connections);
+    tokio::spawn(async move {
+        loop {
+            let accounts = get_users_bybit_streams().await;
+            let mut user_connections = users.lock().unwrap();
+            for ((user_id, stream_id), (key, secret)) in accounts {
+                println!("Connected User {}, with Stream {}", user_id, stream_id);
+                *user_connections
+                    .entry((user_id, stream_id))
+                    .or_insert(AuthenticatedDerivativesAccount::new_with_subscriptions(&key, &secret, vec![String::from("execution")])) 
+                    = AuthenticatedDerivativesAccount::new_with_subscriptions(&key, &secret, vec![String::from("execution")]);
+            }
+            drop(user_connections);
+
+            thread::sleep(time::Duration::from_secs(5*60));
+        }
+
+    });
+
+    // Message sender to Kinesis
+    let (execution_sender, execution_receiver): (SyncSender<(String, String, String)>, _) = sync_channel(2048);
+
+    tokio::spawn(async move {
+        loop {
+            let message = execution_receiver.try_recv();
+            if message.is_err() { continue } // TODO: handle this error
+
+            push_to_kinesis(
+                "signaland-vip-websocket-notifications", 
+                bytes::Bytes::from(serde_json::to_string(&message.unwrap()).unwrap()),
+                "execution_stream"
+            ).await;
+        }
+    });
+
+    // thread to ping pong
+    let (ping_sender, ping_receiver) : (SyncSender<i8>, _) = sync_channel(1);
+    // Execution message is: UserId, StreamId, ByBit Execution Message
+    
+    std::thread::spawn(move || {
+        loop {
+            ping_sender.send(0).unwrap();
+            thread::sleep(time::Duration::from_secs(10));
+        }
+    });
+
+    // Main Loop
+    let socket_connections = Arc::clone(&connections);
+
+    loop {
+        // HeartBeat Ping Pong Websocket Handler
+        let rev_message = ping_receiver.try_recv();
+        let mut user_connections = socket_connections.lock().unwrap();
+        if rev_message.is_ok() {
+            for ((_user_id, _stream_id), auth) in &mut *user_connections {
+                auth.ping();
+            }
+        }
+
+        for ((user_id, stream_id), auth) in &mut *user_connections {
+            let incoming = auth.private_stream.read_message();
+            match incoming {
+                Result::Ok(message) => { 
+                    execution_sender
+                        .send((user_id.clone(), stream_id.clone(), String::from(message.into_text().unwrap())))
+                        .unwrap(); 
+                    () 
+                },
+                Result::Err(e) => { println!("{:?}", e); continue }
+            }
+        }
+
+        drop(user_connections)
+    }
+    
+
+    Ok(())
 }
